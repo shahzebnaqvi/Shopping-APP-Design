6f06848181a9594b7a9aa4e04347b9c950f28ed4
shahzebnaqvi <shahzeb.naqvi12@gmail.com>
2021-11-14 17:17:42 +0500

    sidebar added

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
 
 class Home extends StatefulWidget {
   const Home({Key? key}) : super(key: key);
@@ -13,29 +14,139 @@ class _HomeState extends State<Home> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: Text("Home Page"),
-          actions: [
-            Icon(
-              Icons.favorite,
-              color: Colors.red,
+      drawer: Drawer(
+        // Add a ListView to the drawer. This ensures the user can scroll
+        // through the options in the drawer if there isn't enough vertical
+        // space to fit everything.
+        child: ListView(
+          // Important: Remove any padding from the ListView.
+          padding: EdgeInsets.zero,
+          children: [
+            DrawerHeader(
+                decoration: BoxDecoration(),
+                child: ListTile(
+                  leading: CircleAvatar(
+                    backgroundImage: NetworkImage(
+                        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSDfSwap3iAYqS7YoldaB-ZT92aoKU9KymmtbNV3gjeJ5wI9Wed4AT8jANOQ3C6k3mI_XQ&usqp=CAU"),
+                  ),
+                  title: Text("Username"),
+                  subtitle: Text("abcuser@gmail.com"),
+                )),
+            ListTile(
+              leading: Icon(Icons.logout),
+              title: const Text('Logout'),
+              onTap: () {},
             ),
-            Icon(
-              Icons.favorite,
-              color: Colors.red,
+            ListTile(
+              leading: Icon(Icons.shopping_cart_outlined),
+              title: const Text('Cart'),
+              onTap: () {},
+            ),
+            ListTile(
+              leading: Icon(Icons.favorite),
+              title: const Text('Favourite'),
+              onTap: () {},
+            ),
+            ListTile(
+              leading: Icon(Icons.info),
+              title: const Text('About'),
+              onTap: () {},
             ),
           ],
         ),
-        body: SingleChildScrollView(
-          child: Column(
-            children: [
-              productcard(context, "30% off", Icon(Icons.favorite),
-                  "Black T-Shirt", "\$50", Icon(Icons.shopping_cart)),
-              productcard(context, "30% off", Icon(Icons.favorite),
-                  "Black T-Shirt", "\$50", Icon(Icons.shopping_cart)),
-            ],
+      ),
+      appBar: AppBar(
+        backgroundColor: Colors.white,
+        iconTheme: IconThemeData(color: Colors.red),
+        title: Center(
+            child: Text("Home Page", style: TextStyle(color: Colors.black))),
+        actions: [
+          Icon(
+            Icons.favorite,
+            color: Colors.red,
+            size: 35,
           ),
-        ));
+          SizedBox(width: MediaQuery.of(context).size.width * 0.02),
+          Stack(alignment: Alignment.topCenter, children: [
+            Container(
+              alignment: Alignment.center,
+              child: Icon(
+                Icons.shopping_cart,
+                color: Colors.red,
+                size: 35,
+              ),
+            ),
+            Positioned(
+              left: 15,
+              top: 2,
+              child: CircleAvatar(
+                radius: 10,
+                backgroundColor: Colors.red,
+                child: Text("3"),
+              ),
+            )
+          ]),
+          SizedBox(width: MediaQuery.of(context).size.width * 0.02),
+        ],
+      ),
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            SingleChildScrollView(
+                scrollDirection: Axis.horizontal,
+                child: Row(children: [
+                  productcategory(context, "Shoes", "10 Pieces Left"),
+                  productcategory(context, "Shoes", "10 Pieces Left"),
+                  productcategory(context, "Shoes", "10 Pieces Left"),
+                  productcategory(context, "Shoes", "10 Pieces Left"),
+                ])),
+            productcard(
+                context,
+                "30% off",
+                Icon(
+                  Icons.favorite,
+                  color: Colors.red,
+                ),
+                "Black T-Shirt",
+                "\$50",
+                Icon(
+                  Icons.shopping_cart,
+                  size: 30,
+                )),
+            productcard(
+                context,
+                "30% off",
+                Icon(
+                  Icons.favorite,
+                  color: Colors.red,
+                ),
+                "Black T-Shirt",
+                "\$50",
+                Icon(Icons.shopping_cart)),
+          ],
+        ),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: 'Home',
+            backgroundColor: Colors.red,
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.search),
+            label: 'Search',
+            backgroundColor: Colors.pink,
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.person),
+            label: 'Profile',
+            backgroundColor: Colors.pink,
+          ),
+        ],
+        selectedItemColor: Colors.amber[800],
+      ),
+    );
   }
 }
 
@@ -67,24 +178,69 @@ Widget productcard(context, String dicount, Icon iconfavour, producttitle,
               ],
             ),
             Container(
+              width: MediaQuery.of(context).size.width * 0.5,
+              height: MediaQuery.of(context).size.height * 0.3,
               decoration: BoxDecoration(
                 image: DecorationImage(
                     image: NetworkImage(
-                        "https://5.imimg.com/data5/HB/VQ/MY-44811379/men-black-plain-t-shirt-500x500.jpg"),
+                        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcShLHw-wMFrGfTnBKF3GZJzmllaWHWkWkr48GaXOdxTJ6MQB9EkeM8vudO4TBPA-BfJB5c&usqp=CAU"),
                     fit: BoxFit.cover),
               ),
             ),
             Row(
               mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [Text(producttitle), Text(productprice)],
+              children: [
+                Text(
+                  producttitle,
+                  style: TextStyle(
+                      fontSize: MediaQuery.of(context).size.width * 0.05,
+                      fontWeight: FontWeight.bold),
+                ),
+                Text(productprice,
+                    style: TextStyle(
+                      fontSize: MediaQuery.of(context).size.width * 0.05,
+                    ))
+              ],
             ),
             Row(
               mainAxisAlignment: MainAxisAlignment.end,
               children: [iconcart],
+            ),
+            SizedBox(
+              height: MediaQuery.of(context).size.height * 0.04,
             )
           ]),
         ),
       ),
+    ),
+  );
+}
+
+Widget productcategory(context, categ, categdetail) {
+  return Card(
+    child: Container(
+      padding: EdgeInsets.all(MediaQuery.of(context).size.width * 0.02),
+      child: Row(children: [
+        Image.network(
+          "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcShLHw-wMFrGfTnBKF3GZJzmllaWHWkWkr48GaXOdxTJ6MQB9EkeM8vudO4TBPA-BfJB5c&usqp=CAU",
+          width: MediaQuery.of(context).size.width * 0.2,
+        ),
+        Column(
+          children: [
+            Text(
+              categ,
+              style:
+                  TextStyle(fontSize: MediaQuery.of(context).size.width * 0.05),
+            ),
+            Text(
+              categdetail,
+              style: TextStyle(
+                  fontSize: MediaQuery.of(context).size.width * 0.04,
+                  color: Colors.purple),
+            )
+          ],
+        )
+      ]),
     ),
   );
 }
